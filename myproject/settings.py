9f27e3c5e867d55d24078bb30cd476aa5383d8db
SaumyaKumarDas <dassaumya13@gmail.com>
2026-07-28 23:26:28 +0545

    Configure Gmail SMTP settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -138,3 +138,10 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('dassaumya13@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('iouj ibsd jdco cvvn')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
